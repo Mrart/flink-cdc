635486285fc52bdcfdd3d81dba16cdb93f5675d4
ouyangwulink <mr.artboy@gmail.com>
2024-11-05 09:04:51 +0800

    refactor ticdc

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/tikv/cdc/RegionWorker.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/tikv/cdc/RegionWorker.java
@@ -1,2 +1,21 @@
-package org.tikv.cdc;public class RegionWorker {
+package org.tikv.cdc;
+
+import org.tikv.kvproto.Cdcpb.Event.Row;
+
+public class RegionWorker {
+
+  public RegionFeedEvent assembleRowEvent(long regionId, Row row) {
+    RegionFeedEvent.RawKVEntry rawKVEntry = new RegionFeedEvent.RawKVEntry();
+    rawKVEntry.setOpType(OpType.valueOf(row.getOpType().getNumber()));
+    rawKVEntry.setRegionId(regionId);
+    rawKVEntry.setKey(row.getKey());
+    rawKVEntry.setValue(row.getValue());
+    rawKVEntry.setStartTs(row.getStartTs());
+    rawKVEntry.setCrts(row.getCommitTs());
+    rawKVEntry.setOldValue(row.getOldValue());
+    RegionFeedEvent reEvent = new RegionFeedEvent();
+    reEvent.setRegionId(regionId);
+    reEvent.setRawKVEntry(rawKVEntry);
+    return reEvent;
+  }
 }
