6473ab9b995bdd2d6dd696f4a24c17a4068ba5ae
wulin <mr.artboy@gmail.com>
2024-11-20 09:45:27 +0800

    1120

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/apache/flink/cdc/connectors/tidb/source/offset/CDCEventOffset.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/apache/flink/cdc/connectors/tidb/source/offset/CDCEventOffset.java
@@ -16,11 +16,6 @@ public class CDCEventOffset extends Offset {
       new CDCEventOffset(Collections.singletonMap(TIMESTAMP_KEY, "0"));
   public static final CDCEventOffset NO_STOPPING_OFFSET = new CDCEventOffset(Long.MAX_VALUE);
 
-  public static final String BINLOG_FILENAME_OFFSET_KEY = "file";
-  public static final String BINLOG_POSITION_OFFSET_KEY = "pos";
-  public static final String ROWS_TO_SKIP_OFFSET_KEY = "row";
-  public static final String GTID_SET_KEY = "gtids";
-
   public CDCEventOffset(Map<String, ?> offset) {
     Map<String, String> offsetMap = new HashMap<>();
     for (Map.Entry<String, ?> entry : offset.entrySet()) {
@@ -41,26 +36,17 @@ public class CDCEventOffset extends Offset {
   }
 
   public CDCEventOffset(
-          String filename,
-          long position,
-          long restartSkipEvents,
-          long restartSkipRows,
-          long binlogEpochSecs,
-          @Nullable String restartGtidSet) {
+      String filename,
+      long position,
+      long restartSkipEvents,
+      long restartSkipRows,
+      long binlogEpochSecs,
+      @Nullable String restartGtidSet) {
     Map<String, String> offsetMap = new HashMap<>();
-    offsetMap.put(BINLOG_FILENAME_OFFSET_KEY, filename);
-    offsetMap.put(BINLOG_POSITION_OFFSET_KEY, String.valueOf(position));
     offsetMap.put(EVENTS_TO_SKIP_KEY, String.valueOf(restartSkipEvents));
-    offsetMap.put(ROWS_TO_SKIP_OFFSET_KEY, String.valueOf(restartSkipRows));
     offsetMap.put(TIMESTAMP_KEY, String.valueOf(binlogEpochSecs));
-    if (restartGtidSet != null) {
-      offsetMap.put(GTID_SET_KEY, restartGtidSet);
-    }
     this.offset = offsetMap;
   }
-
-
-
 
   public String getTimestamp() {
     return offset.get(TIMESTAMP_KEY);
