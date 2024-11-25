657e50289152a59f40377c32c324014ebd17d4db
wulin <mr.artboy@gmail.com>
2024-11-25 16:22:23 +0800

    jdbc properties

--- flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/tikv/cdc/CDCClientV2.java
+++ flink-cdc-connect/flink-cdc-source-connectors/flink-connector-tidb-cdc/src/main/java/org/tikv/cdc/CDCClientV2.java
@@ -1,7 +1,14 @@
 package org.tikv.cdc;
 
+import io.debezium.connector.tidb.TiDBPartition;
+import io.debezium.pipeline.source.spi.ChangeEventSource;
+import org.apache.flink.cdc.connectors.tidb.source.offset.CDCEventOffsetContext;
+
 public interface CDCClientV2 {
-  void start();
+  void execute(
+      ChangeEventSource.ChangeEventSourceContext changeEventSourceContext,
+      TiDBPartition tiDBPartition,
+      CDCEventOffsetContext offsetContext);
 
   long getResolvedTs();
 
